ac4bffca7e6b9de447d367500c42273b24eeea0e
chaos29092 <chaos29092@gmail.com>
2015-04-06 14:08:26 +0800

    price list done

--- app/views/emails/price.blade.php
+++ app/views/emails/price.blade.php
@@ -1,70 +1,152 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-	</head>
-	<body>
-		<h2>Price List：</h2>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+<head>
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>ZENO Price List</title>
 
-		<div>
-			<h4>Laptop Diagnostic Ultrasound Scanner</h4>
-            <p><strong>Price (FOB): $ 2,990</strong></p>
-            <h5>Main characteristic</h5>
-            <ol>
-                <li>12.1 inch TFT color LCD</li>
-                <li>1 probe standard</li>
-                <li>Black & White</li>
-            </ol>
-            <hr/>
 
-            <h4>Palmsize Diagnostic Ultrasound Scanner(20% off now!)</h4>
-            <p><strong>Price (FOB): $ 4,150(20% off now,$3,300)</strong></p>
-            <h5>Main characteristic</h5>
-            <ol>
-                <li>10.4 inch Touch Screen</li>
-                <li>3D reconstruction function</li>
-                <li>Imaging Modes: B, B/B, B/M, M, 4B, 9B</li>
-                <li>Pseudo-color</li>
-            </ol>
-            <hr/>
+<style type="text/css">
+img {
+max-width: 100%;
+}
+body {
+-webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6;
+}
+body {
+background-color: #f6f6f6;
+}
+@media only screen and (max-width: 640px) {
+  h1 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h2 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h3 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h4 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h1 {
+    font-size: 22px !important;
+  }
+  h2 {
+    font-size: 18px !important;
+  }
+  h3 {
+    font-size: 16px !important;
+  }
+  .container {
+    width: 100% !important;
+  }
+  .content {
+    padding: 10px !important;
+  }
+  .content-wrap {
+    padding: 10px !important;
+  }
+  .invoice {
+    width: 100% !important;
+  }
+}
+</style>
+</head>
 
-            <h4>Portable Ultrasound Color Doppler</h4>
-            <p><strong>Price (FOB): $ 9,980</strong></p>
-            <h5>Main characteristic</h5>
-            <ol>
-                <li>15 inch LCD screen Color Doppler</li>
-                <li>Compact color doppler ultrasound system</li>
-                <li>Imaging Modes: B, B/B, B/M, M, 4B, C, PDI, PW</li>
-                <li>2 probes standard</li>
-            </ol>
-            <hr/>
+<body style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6; background: #f6f6f6; margin: 0; padding: 0;">
 
-            <h4>Laptop Ultrasound Color Doppler</h4>
-            <p><strong>Price (FOB): $ 9,800</strong></p>
-            <h5>Main characteristic</h5>
-            <ol>
-                <li>15 inch LED Screen Color Doppler;</li>
-                <li>3D imaging(option);</li>
-                <li>Imaging Modes: B, B/B, B/M, M, 4B, C, PDI, PW;</li>
-                <li>2 probes standard</li>
-            </ol>
-            <hr/>
+<table class="body-wrap" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background: #f6f6f6; margin: 0; padding: 0;">
+	<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+		<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;" valign="top"></td>
+		<td class="container" width="600" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;" valign="top">
+			<div class="content" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+				<table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background: #fff; margin: 0; padding: 0; border: 1px solid #e9e9e9;">
+					<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+						<td class="content-wrap aligncenter" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: center; margin: 0; padding: 20px;" align="center" valign="top">
+							<table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+								<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+									<td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										<h1 style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; box-sizing: border-box; font-size: 32px; color: #000; line-height: 1.2; font-weight: 500; margin: 40px 0 0; padding: 0;">ZENO Price List</h1>
+									</td>
+								</tr>
+								<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+									<td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										<h2 style="font-family: 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; box-sizing: border-box; font-size: 24px; color: #000; line-height: 1.2; font-weight: 400; margin: 40px 0 0; padding: 0;">Price of Ultrasound Devices(FOB)</h2>
+									</td>
+								</tr>
+								<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+									<td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										<table class="invoice" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; text-align: left; width: 80%; margin: 40px auto; padding: 0;">
+											<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+												<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 5px 0;" valign="top">April 01 2015</td>
+											</tr>
+											<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+												<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 5px 0;" valign="top">
+													<table class="invoice-items" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; margin: 0; padding: 0;">
+														<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+															<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">
+                                                                <a href="http://zeno-med.com/nocolordevice/laptop" target="_blank">Laptop Diagnostic Ultrasound Scanner</a>
+                                                            </td>
+															<td class="alignright" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">$ 2,990</td>
+														</tr>
+														<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+															<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">
+                                                                <a href="http://zeno-med.com/nocolordevice/ipad" target="_blank">Palmsize Diagnostic Ultrasound Scanner <br/>(20% off now! from $ 4,150)</a></td>
+															<td class="alignright" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">$ 3,300 <br/>
+                                                                ($ 4,150)</td>
+														</tr>
+														<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+															<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">
+                                                                <a href="http://zeno-med.com/colordevice/portable" target="_blank">Portable Ultrasound Color Doppler</a>
+                                                            </td>
+															<td class="alignright" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">$ 9,980</td>
+														</tr>
+                                                        <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+															<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">
+                                                                <a href="http://zeno-med.com/colordevice/laptop" target="_blank">Laptop Ultrasound Color Doppler</a>
+                                                            </td>
+															<td class="alignright" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">$ 9,800</td>
+														</tr>
+                                                        <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+															<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">
+                                                                <a href="http://zeno-med.com/colordevice/ipad" target="_blank">Palmsize Ultrasound Color Doppler</a>
+                                                            </td>
+															<td class="alignright" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">$ 10,500</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+								<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+									<td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										<a href="http://www.zeno-med.com" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; color: #348eda; text-decoration: underline; margin: 0; padding: 0;">View our website</a>
+									</td>
+								</tr>
+								<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+									<td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										ZENO Inc. Hi-tech Zone, Zhengzhou City, Henan Province, China
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+				<div class="footer" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;">
+					<table width="100%" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+						<tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+							<td class="aligncenter content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">Questions? Email <a href="mailto:info@zeno-med.com" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0; padding: 0;">info@zeno-med.com</a>
+                                <br/>OR Tel: 0086 - 18538153651
+                            </td>
+						</tr>
+					</table>
+				</div></div>
+		</td>
+		<td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;" valign="top"></td>
+	</tr>
+</table>
 
-            <h4>Palmsize Ultrasound Color Doppler</h4>
-            <p><strong>Price (FOB): $ 10,500</strong></p>
-            <h5>Main characteristic</h5>
-            <ol>
-                <li>22 inch color LED Touch Screen Color Doppler;</li>
-                <li>With multiple probes to fit different uses and budgets;</li>
-                <li>Imaging Mode: B, BB, M, CD, PWD, CWD, Dir Pwr, Pwr;</li>
-                <li>2 probes standard</li>
-            </ol>
-            <hr/>
-
-            <p>Facebook: mariadoppler-1984@hotmail.com</p>
-            <p>Email: info@zeno-med.com</p>
-            <p>Website: www.zeno-med.com</p>
-            <p>Tel: 0086- 18538153651</p>
-		</div>
-	</body>
+</body>
 </html>
---
+++ app/views/emails/price1.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Price List：</h2>
+
+		<div>
+			<h4>Laptop Diagnostic Ultrasound Scanner</h4>
+            <p><strong>Price (FOB): $ 2,990</strong></p>
+            <h5>Main characteristic</h5>
+            <ol>
+                <li>12.1 inch TFT color LCD</li>
+                <li>1 probe standard</li>
+                <li>Black & White</li>
+            </ol>
+            <hr/>
+
+            <h4>Palmsize Diagnostic Ultrasound Scanner(20% off now!)</h4>
+            <p><strong>Price (FOB): $ 4,150(20% off now,$3,300)</strong></p>
+            <h5>Main characteristic</h5>
+            <ol>
+                <li>10.4 inch Touch Screen</li>
+                <li>3D reconstruction function</li>
+                <li>Imaging Modes: B, B/B, B/M, M, 4B, 9B</li>
+                <li>Pseudo-color</li>
+            </ol>
+            <hr/>
+
+            <h4>Portable Ultrasound Color Doppler</h4>
+            <p><strong>Price (FOB): $ 9,980</strong></p>
+            <h5>Main characteristic</h5>
+            <ol>
+                <li>15 inch LCD screen Color Doppler</li>
+                <li>Compact color doppler ultrasound system</li>
+                <li>Imaging Modes: B, B/B, B/M, M, 4B, C, PDI, PW</li>
+                <li>2 probes standard</li>
+            </ol>
+            <hr/>
+
+            <h4>Laptop Ultrasound Color Doppler</h4>
+            <p><strong>Price (FOB): $ 9,800</strong></p>
+            <h5>Main characteristic</h5>
+            <ol>
+                <li>15 inch LED Screen Color Doppler;</li>
+                <li>3D imaging(option);</li>
+                <li>Imaging Modes: B, B/B, B/M, M, 4B, C, PDI, PW;</li>
+                <li>2 probes standard</li>
+            </ol>
+            <hr/>
+
+            <h4>Palmsize Ultrasound Color Doppler</h4>
+            <p><strong>Price (FOB): $ 10,500</strong></p>
+            <h5>Main characteristic</h5>
+            <ol>
+                <li>22 inch color LED Touch Screen Color Doppler;</li>
+                <li>With multiple probes to fit different uses and budgets;</li>
+                <li>Imaging Mode: B, BB, M, CD, PWD, CWD, Dir Pwr, Pwr;</li>
+                <li>2 probes standard</li>
+            </ol>
+            <hr/>
+
+            <p>Facebook: mariadoppler_1984@hotmail.com</p>
+            <p>Email: info@zeno-med.com</p>
+            <p>Website: www.zeno-med.com</p>
+            <p>Tel: 0086 - 18538153651</p>
+		</div>
+	</body>
+</html>
